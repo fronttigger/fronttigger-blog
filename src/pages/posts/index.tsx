d7e93d589661ff2bf17d5eb9ae6884aa10e016f2
tigger <fronttigger@gmail.com>
2022-05-25 00:55:03 +0900

    posts 화면 최초 처리

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,25 +2,19 @@ import { useEffect } from 'react'
 import { GetStaticProps } from 'next'
 import Link from 'next/link'
 import { InView } from 'react-intersection-observer'
-import { useSetRecoilState, useRecoilValue, useRecoilState } from 'recoil'
+import { useSetRecoilState, useRecoilValue } from 'recoil'
 import config from 'config'
 
 import Skeleton from '#components/skeleton'
 import * as styles from '#shared/styles/pages/posts/styles.css'
 import PageSEO from '#components/shared/SEO/PageSEO'
 import Card from '#components/card'
-import {
-  hasNextPostsPageState,
-  allPostsState,
-  pageState,
-  currentPostsState,
-  initPostsPageLoading,
-} from '#store/posts'
+import { hasNextPostsPageState, allPostsState, pageState, currentPostsState } from '#store/posts'
 import { Post } from '#types/post'
 import { getAllPosts } from '#utils/posts'
+import { DEFAULT_POSTS_SIZE } from '#constants'
 
 function PostsPage({ allPosts }: { allPosts: Post[] }) {
-  const [isPostsPageLoading, setIsPostsPageLoading] = useRecoilState(initPostsPageLoading)
   const currentPosts = useRecoilValue(currentPostsState)
   const hasNextPostsPage = useRecoilValue(hasNextPostsPageState)
   const setPosts = useSetRecoilState(allPostsState)
@@ -32,14 +26,6 @@ function PostsPage({ allPosts }: { allPosts: Post[] }) {
     }
   }
 
-  useEffect(() => {
-    if (isPostsPageLoading) {
-      setIsPostsPageLoading(false)
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
   useEffect((): void => setPosts(allPosts), [allPosts, setPosts])
 
   return (
@@ -47,24 +33,28 @@ function PostsPage({ allPosts }: { allPosts: Post[] }) {
       <PageSEO title="Posts" url={config.url} />
       <h1 className={styles.title}>Posts</h1>
       <ul className={styles.container}>
-        {currentPosts.map(({ slug, frontMatter }, index) => (
-          <InView
-            key={index}
-            as="li"
-            className={styles.cardContainer}
-            onChange={(InView) => handleChangeInview(InView, index)}
-          >
-            {isPostsPageLoading ? (
-              <Skeleton />
-            ) : (
-              <Link href={`/${slug.year}/${slug.subject}/${slug.title}`}>
-                <a>
-                  <Card frontMatter={frontMatter} />
-                </a>
-              </Link>
-            )}
-          </InView>
-        ))}
+        {currentPosts.length > 0
+          ? currentPosts.map(({ slug, frontMatter }, index) => (
+              // eslint-disable-next-line react/jsx-indent
+              <InView
+                key={index}
+                as="li"
+                className={styles.cardContainer}
+                onChange={(InView) => handleChangeInview(InView, index)}
+              >
+                <Link href={`/${slug.year}/${slug.subject}/${slug.title}`}>
+                  <a>
+                    <Card frontMatter={frontMatter} />
+                  </a>
+                </Link>
+              </InView>
+            ))
+          : new Array(DEFAULT_POSTS_SIZE).fill(0).map((_, index) => (
+              // eslint-disable-next-line react/jsx-indent
+              <li key={index} className={styles.cardContainer}>
+                <Skeleton />
+              </li>
+            ))}
       </ul>
     </>
   )
